d2683ec395f54f8722e515000c2d4e0243bd0c38
eyal0 <109809+eyal0@users.noreply.github.com>
2020-12-09 12:25:34 -0700

    Require OctoPrint>=1.5.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/eyal0/OctoPrint-TimeToFilament"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["OctoPrint-PrintTimeGenius"]
+plugin_requires = ["OctoPrint-PrintTimeGenius", "OctoPrint>=1.5.0"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
