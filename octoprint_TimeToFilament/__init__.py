eyal0/OctoPrint-TimeToFilament

commit c7425eeb25bb5b287439084fdb6001e7131282aa

Use additional_plugin_data hook instead of monkey patching progress

octoprint_TimeToFilament/__init__.py
@@ -31,11 +31,11 @@ class TimeToFilamentPlugin(octoprint.plugin.SettingsPlugin,
             {"enabled": True,
              "description": "Time to Next Layer",
              "regex": "^; layer (\\d+)",
-             "format": 'Layer ${this.progress.TimeToFilament["^; layer (\\\\d+)"].groups[0]} in <b>${formatDuration(this.progress.printTimeLeft - this.progress.TimeToFilament["^; layer (\\\\d+)"].timeLeft)}</b>'},
+             "format": 'Layer ${this.plugins.TimeToFilament["^; layer (\\\\d+)"].groups[0]} in <b>${formatDuration(this.progress.printTimeLeft - this.plugins.TimeToFilament["^; layer (\\\\d+)"].timeLeft)}</b>'},
             {"enabled": True,
              "description": "Time to Next Filament Change",
              "regex": "^M600",
-             "format": 'Filament change in <b>${formatDuration(this.progress.printTimeLeft - this.progress.TimeToFilament["^M600"].timeLeft)}</b>'}
+             "format": 'Filament change in <b>${formatDuration(this.progress.printTimeLeft - this.plugins.TimeToFilament["^M600"].timeLeft)}</b>'}
         ]
     }
 
@@ -49,68 +49,6 @@ class TimeToFilamentPlugin(octoprint.plugin.SettingsPlugin,
 
     self._logger.addHandler(logging_handler)
     self._logger.propagate = False
-
-    def newUpdateProgressDataCallback(old_callback, printer):
-      def return_function(state_monitor):
-        old_result = dd()
-        # Get the results from the original callback, to be updated.
-        old_result.update(old_callback())
-        try:
-          # Can we use the cached result?
-          if printer._comm._currentFile is not self._cached_currentFile:
-            # No, we need to clear out the cache.
-            self._cached_results = dd()
-            self._cached_currentFile = printer._comm._currentFile
-          file_pos = printer._comm._currentFile.getFilepos()
-          for regex, cached_result in list(self._cached_results.items()):
-            if (file_pos > cached_result["matchPos"] or
-                file_pos < cached_result["searchPos"]):
-              del self._cached_results[regex]
-          old_result["TimeToFilament"].update(self._cached_results)
-          regexes = set(x["regex"]
-                        for x in self._settings.get(["displayLines"])
-                        if x["enabled"] and (x["regex"] not in old_result["TimeToFilament"].keys()))
-          if regexes:
-            with open(printer._comm._currentFile.getFilename()) as gcode_file:
-              gcode_file.seek(file_pos)
-              # Now search forward for the regex.
-              while regexes:
-                line = gcode_file.readline()
-                if not line:
-                  # Ran out of lines and didn't find anything more.
-                  for regex in list(regexes):
-                    self._cached_results[regex]["matchPos"] = float("inf")
-                    self._cached_results[regex]["searchPos"] = file_pos
-                  break
-                for regex in list(regexes): # Make a copy because we modify it.
-                  m = re.search(regex, line)
-                  if m:
-                    match_pos = gcode_file.tell()
-                    timeLeft, timeOrigin = printer._estimator.estimate(
-                        float(match_pos) / printer._comm._currentFile.getFilesize(),
-                        None, None, None, None)
-                    self._cached_results[regex] = {
-                        "timeLeft": timeLeft,
-                        "groups": m.groups(),
-                        "group": m.group(),
-                        "groupdict": m.groupdict(),
-                        "matchPos": match_pos,
-                        "searchPos": file_pos,
-                    }
-                    regexes.remove(regex)
-          old_result["TimeToFilament"].update(self._cached_results)
-        except Exception as e:
-          print("Failed: " + repr(e))
-        for regex in list(old_result["TimeToFilament"].keys()):
-          if old_result["TimeToFilament"][regex]["matchPos"] == float("inf"):
-            del old_result["TimeToFilament"][regex]
-        if (time.time() > self._last_debug + 10):
-          self._logger.info("sending: " + json.dumps(old_result))
-          self._last_debug = time.time()
-        return old_result
-      return return_function
-    self._printer._stateMonitor._on_get_progress = types.MethodType(newUpdateProgressDataCallback(
-        self._printer._stateMonitor._on_get_progress, self._printer), self._printer._stateMonitor)
 
   @octoprint.plugin.BlueprintPlugin.route("/get_settings_defaults", methods=["GET"])
   def get_settings_defaults_as_string(self):
@@ -148,6 +86,61 @@ class TimeToFilamentPlugin(octoprint.plugin.SettingsPlugin,
         )
     )
 
+  def additional_state_data(self, initial, *args, **kwargs):
+    try:
+      if not self._printer._comm._currentFile:
+        return None
+      # Can we use the cached result?
+      if self._printer._comm._currentFile is not self._cached_currentFile:
+        # No, we need to clear out the cache.
+        self._cached_results = dd()
+        self._cached_currentFile = self._printer._comm._currentFile
+      file_pos = self._printer._comm._currentFile.getFilepos()
+      for regex, cached_result in list(self._cached_results.items()):
+        if (file_pos > cached_result["matchPos"] or
+            file_pos < cached_result["searchPos"]):
+          del self._cached_results[regex]
+      regexes = set(x["regex"]
+                    for x in self._settings.get(["displayLines"])
+                    if x["enabled"] and (x["regex"] not in self._cached_results.keys()))
+      if regexes:
+        with open(self._printer._comm._currentFile.getFilename()) as gcode_file:
+          gcode_file.seek(file_pos)
+          # Now search forward for the regex.
+          while regexes:
+            line = gcode_file.readline()
+            if not line:
+              # Ran out of lines and didn't find anything more.
+              for regex in list(regexes):
+                self._cached_results[regex]["matchPos"] = float("inf")
+                self._cached_results[regex]["searchPos"] = file_pos
+              break
+            for regex in list(regexes): # Make a copy because we modify it.
+              m = re.search(regex, line)
+              if m:
+                match_pos = gcode_file.tell()
+                time_left, _ = self._printer._estimator.estimate(
+                    float(match_pos) / self._printer._comm._currentFile.getFilesize(),
+                    None, None, None, None)
+                self._cached_results[regex] = {
+                    "timeLeft": time_left,
+                    "groups": m.groups(),
+                    "group": m.group(),
+                    "groupdict": m.groupdict(),
+                    "matchPos": match_pos,
+                    "searchPos": file_pos,
+                }
+                regexes.remove(regex)
+      for regex in list(self._cached_results.keys()):
+        if self._cached_results[regex]["matchPos"] == float("inf"):
+          del self._cached_results[regex]
+      if (time.time() > self._last_debug + 10):
+        self._logger.info("sending: %s", json.dumps(self._cached_results))
+        self._last_debug = time.time()
+      return self._cached_results
+    except Exception as e:
+      self._logger.error("Failed: %s", repr(e))
+    return None
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
@@ -162,5 +155,6 @@ def __plugin_load__():
 
   global __plugin_hooks__
   __plugin_hooks__ = {
-      "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+      "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+      "octoprint.printer.additional_state_data": __plugin_implementation__.additional_state_data
   }

octoprint_TimeToFilament/static/js/TimeToFilament.js
@@ -57,7 +57,8 @@ $(function() {
         }
       }
       progress.completion = progress.filepos / self.sampleGcode().length;
-      progress.TimeToFilament = {};
+      plugins = {};
+      plugins.TimeToFilament = {};
       for (const displayLine of self.displayLines()) {
         const regex = displayLine.regex();
         const format = displayLine.format();
@@ -65,7 +66,7 @@ $(function() {
         let timeSoFar = 0;
         for (let line of self.sampleGcode().substr(progress.filepos).split("\n")) {
           if (m = line.match(regex)) {
-            progress.TimeToFilament[regex] = {
+            plugins.TimeToFilament[regex] = {
               "group": m[0],
               "groupdict": m.groups,
               "groups": m.slice(1),
@@ -78,12 +79,12 @@ $(function() {
           timeSoFar += [...line].reduce((a,b) => a+b.charCodeAt(0), 0);
         }
       }
-      return {"progress": progress};
+      return {"progress": progress, "plugins": plugins};
     });
 
     self.sampleFormatOutput = function(displayLineIndex) {
-      if (!(self.displayLines()[displayLineIndex()].regex() in self.sampleOutput()["progress"]["TimeToFilament"])) {
-        return "'" + self.displayLines()[displayLineIndex()].regex() + "' not found in this.progress.TimeToFilament";
+      if (!(self.displayLines()[displayLineIndex()].regex() in self.sampleOutput()["plugins"]["TimeToFilament"])) {
+        return "'" + self.displayLines()[displayLineIndex()].regex() + "' not found in this.plugins.TimeToFilament";
       }
       try {
         const fillTemplate = function(templateString, templateVars){
@@ -96,7 +97,7 @@ $(function() {
     }
 
     self.fromCurrentData = function(data) {
-      if (!("progress" in data) || !("TimeToFilament" in data["progress"])) {
+      if (!("plugins" in data) || !("TimeToFilament" in data["plugins"])) {
         let div = document.getElementById("TimeToFilament");
         if (div) {
           div.style.display = "none";
@@ -107,7 +108,7 @@ $(function() {
       const displayLines = self.displayLines();
       for (const displayLine of displayLines) {
         const regex = displayLine.regex();
-        if (regex in data["progress"]["TimeToFilament"]) {
+        if (regex in data["plugins"]["TimeToFilament"]) {
           let found = document.getElementById("TimeToFilament-" + regex);
           if (!found) {
             let newLine = document.createElement("span");
@@ -117,7 +118,7 @@ $(function() {
           }
           // Back to default which is probably to show it.
           document.getElementById("TimeToFilament").style.display = "";
-          result = data["progress"]["TimeToFilament"][regex];
+          result = data["plugins"]["TimeToFilament"][regex];
           const fillTemplate = function(templateString, templateVars){
             return new Function(`return \`${templateString}\`;`).call(templateVars);
           }

commit 06a55cfcbad3cb38d93f2d6bde2e678836e70fc9

Remove Infinities from sent result but not from cache
Removing them from the cache causes the file to be searched again each time, which is very slow.

This is a regression caused by c7425eeb25bb5b287439084fdb6001e7131282aa

This fixes #8

octoprint_TimeToFilament/__init__.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import copy
 import types
 import re
 import octoprint.plugin
@@ -131,13 +132,14 @@ class TimeToFilamentPlugin(octoprint.plugin.SettingsPlugin,
                     "searchPos": file_pos,
                 }
                 regexes.remove(regex)
-      for regex in list(self._cached_results.keys()):
-        if self._cached_results[regex]["matchPos"] == float("inf"):
-          del self._cached_results[regex]
+      ret = copy.deepcopy(self._cached_results)
+      for regex in list(ret.keys()): # Make a copy because we will modify ret
+        if ret[regex]["matchPos"] == float("inf"):
+          del ret[regex]
       if (time.time() > self._last_debug + 10):
-        self._logger.info("sending: %s", json.dumps(self._cached_results))
+        self._logger.info("sending: %s", json.dumps(ret))
         self._last_debug = time.time()
-      return self._cached_results
+      return ret
     except Exception as e:
       self._logger.error("Failed: %s", repr(e))
     return None